512a49056a3f013c277737db0387b104e5511ff7
b0ho <ybg4828@gmail.com>
2019-02-08 20:11:54 +0900

    sets

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div className="App">HI!!</div>;
+    return <div className="App">HI!!!</div>;
   }
 }
 
